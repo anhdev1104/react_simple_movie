7579cc55a81e764831ae66afc567c5c5a97bc23a
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-04-21 00:27:14 +0700

    update movie details page

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -6,9 +6,9 @@ const MovieDetailsPage = () => {
   const { movieId } = useParams();
   const { data } = useSWR(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`, fetcher);
   if (!data) return null;
-  const { backdrop_path, poster_path } = data;
+  const { backdrop_path, poster_path, title, genres, overview } = data;
   return (
-    <>
+    <div className="pb-10">
       <div className="w-full h-[600px] relative">
         <div className="absolute inset-0 bg-black bg-opacity-70"></div>
         <div
@@ -18,14 +18,25 @@ const MovieDetailsPage = () => {
           }}
         ></div>
       </div>
-      <div className="w-full h-[400px] max-w-[800px] mx-auto -translate-y-2/4">
+      <div className="w-full h-[600px] max-w-[460px] mx-auto -mt-[300px] z-10 relative pb-10">
         <img
           src={`https://images.tmdb.org/t/p/original/${poster_path}`}
           alt=""
-          className="w-full h-full object-cover rounded-xl"
+          className="w-full h-full object-contain rounded-xl"
         />
       </div>
-    </>
+      <h1 className="text-center text-3xl font-bold mb-10 text-white">{title}</h1>
+      {genres.length > 0 && (
+        <div className="flex justify-center items-center gap-x-5 mb-10">
+          {genres.map(item => (
+            <span className="py-2 px-4 border-primary border text-primary rounded" key={item.id}>
+              {item.name}
+            </span>
+          ))}
+        </div>
+      )}
+      <p className="text-center leading-relaxed mx-auto max-w-[600px]">{overview}</p>
+    </div>
   );
 };
 
